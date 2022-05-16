ce1ba81f7074d3962098114ac4ca8b936a17b848
Sil <silvanvb@ziggo.nl>
2022-05-16 10:34:09 +0200

    new reservation form, styled

--- loginAzalpenak/insertNewReservationForm.php
+++ loginAzalpenak/insertNewReservationForm.php
@@ -5,169 +5,128 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> -->
-    <title>New Purchase Page</title>
+    <!-- bootstrap 4.6 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+    <!-- scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="newreservationform.css">
 
 
+
+    <title>New Reservation</title>
+
+    <style>
+        body {
+            max-width: 100%;
+            overflow-x: hidden;
+            font-family: 'Inter', sans-serif;
+            background-color: #53667A;
+        }
+
+        #main {
+            /* width: 200px;
+            height: 200px; 
+            border: 1px solid #c3c3c3; */
+            width: 100%;
+            height: 80%;
+            border: 0px;
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        #main div {
+            width: 50px;
+            height: 750px;
+            /* height: 200px; */
+        }
+    </style>
 </head>
 
 <body>
-
+    <?php
+    include("test_connect_db.php");
+    session_start();
+    $currentUser = $_SESSION['usuario'];
+    $link = connectDataBase();
+    $kontsulta1 = mysqli_query($link, "select * from cabin");
+    $kontsulta2 = mysqli_query($link, "select * from reservation where cust_Username = '$currentUser'");
+    ?>
 
     <!-- style='width:600px;float:right;margin-top:50px; margin-right:300px' -->
-    <div style='width:600px;margin-top:50px; margin-right:300px; margin-left:50px'>
-        <!-- style='margin:auto;width:600px;margin-top:50px;' -->
-        <p>usar el mismo estilo que Delete Purchase</p>
-        <h1>Add a new Reservation:</h1>
-        <!--<form action="insertNewReservation.php" method="POST"> -->
-        <form action="insertNewReservation.php" method="POST">
-            <table>
-                <tr>
-                    <td>Customer's username:</td>
-                    <td>
-                        <input name="username" type="text" size='4'><br>
-                    </td>
-                    <!-- <td>Password:<input name="password" type="password" size='8'></td> -->
-                </tr>
-                <tr>
-                    <td>Cabin: [C1, C2, C3, C4]</td>
-                    <td><input name="cabin_id" type="text"><br></td>
-                </tr>
-                <td>
-                    <!-- https://stackoverflow.com/questions/47634469/how-to-populate-dropdown-menu-from-sql-table-in-php -->
-                    <form>
-                        <select name="cabinChoices" class="custom-select mb-3">
-                            <option selected>...</option>
-                            <option value="C1">C1</option>
-                            <option value="C2">C2</option>
-                            <option value="C3">C3</option>
-                            <option value="C4">C4</option>
-                        </select>
-                    </form>
-                </td>
-                <tr>
-                    <td>
-                        <form>
-                            <select name="selectCabin" class="custom-select mb-3">
-                                <option selected>...</option> 
-                                <?php
-                                include("test_connect_db.php");
-                                $link = connectDataBase();
-                                $sql = mysqli_query($link, "select distinct Cabin_ID from cabin order by 1 asc");
-                                ?>
+    <div class="p-5 mx-5">
 
-                                <!-- <option select name='ccc' value="<?php echo $row['Cabin_ID']; ?>"><?php echo $row['Cabin_ID']; ?></option> -->
-                                
-                                <?php
-                                while ($row = mysqli_fetch_array($sql)) {
-                                        ?>
-                                        
-                                        <option value="<?php echo $row['Cabin_ID']; ?>"><?php echo $row['Cabin_ID']; ?></option>
-                                        <?php
-                                        
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <h1 class="font-weight-bold text-white m-0 tracking">Add a new reservation:</h1>
+                <p class="font-weight-bold gray-light h5 tracking">Fill out your info to make a reservation</p>
+            </div>
+            <div>
+                <a href="../loginazalpenak">
+                    <button class="btn btn-warning text-white">Atzera</button>
+                </a>
+            </div>
+        </div>
+        <hr>
 
-                                    }
-                                ?>
-                                
+        <form id="form-container" action="insertNewReservation.php" method="POST">
+            <!-- customer / cabin -->
+            <div class="form-row w-50 mt-5">
+                <div class="col form-group">
+                    <label for="currentUser" class="text-uppercase font-weight-bold gray-light tracking fs-6">Customer</label>
+                    <input type="text" id="currentUser" class="form-control form-bg-blue text-white" disabled placeholder="<?php echo $currentUser ?>">
+                </div>
+                <div class="col form-group">
+                    <label for="currentUser" class="text-uppercase font-weight-bold gray-light tracking fs-6">Cabin</label>
+                    <select name="cabinChoices" class="custom-select form-bg-blue text-white mb-3">
+                        <option selected>Choose a cabin</option>
+                        <option value="C1">C1</option>
+                        <option value="C2">C2</option>
+                        <option value="C3">C3</option>
+                        <option value="C4">C4</option>
+                    </select>
+                </div>
+            </div>
 
-                                
+            <!-- purchase date / hours -->
+            <div class="form-row w-50">
+                <div class="col form-group">
+                    <label for="datepicker" class="text-uppercase font-weight-bold gray-light tracking fs-6">purchase date</label>
+                    <input id="datepicker" type="date" name="reservationDate" placeholder="specify date" class="formAlign custom-select-date" />
+                </div>
+                <div class="col form-group">
+                    <label for="currentUser" class="text-uppercase font-weight-bold gray-light tracking fs-6">Amount of hours</label>
+                    <input name="amount" type="number" class="custom-select-date">
+                </div>
+            </div>
+            <!-- start hours -->
+            <div class="form-row w-50 mt-3">
+                <div class="col form-group">
+                    <label for="start_hour" class="text-uppercase font-weight-bold gray-light tracking fs-6">Start hour</label>
+                    <input name="start_hour" type="time" class="custom-select-date">
+                </div>
+            </div>
+            <!-- end hours -->
+            <div class="form-row w-50 mt-1">
+                <div class="col form-group">
+                    <label for="end_hour" class="text-uppercase font-weight-bold gray-light tracking fs-6">Ending hour</label>
+                    <input name="end_hour" type="time" class="custom-select-date">
+                    <small class="form-text gray-light font-weight-bold tracking">Click on the clock icon to select a time!</small>
 
-                                
-                            </select>
-
-                        </form>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Purchase's Date:</td>
-                    <td><input id="datepicker" type="date" name="reservationDate" placeholder="specify date" class="formAlign" style="display: table-cell;" /></td>
-                </tr>
-                <tr>
-                    <td>Amount of hours:</td>
-                    <td><input name="amount" type="number"><br></td>
-                </tr>
-                <tr>
-                    <td>Starting Hour:</td>
-                    <td><input name="start_hour" type="time"><br></td>
-                </tr>
-                <tr>
-                    <td>Ending Hour:</td>
-                    <td><input name="end_hour" type="time"><br></td>
-                </tr>
-                <tr>
-                    <td><br><input name="send" type="submit" value="Add Reservation"></td>
-                    <td><br><input type="reset" value="Clear"></td>
-                </tr>
-                <!-- echo "Today is " . date("Y-m-d") . "<br>"; -->
-            </table>
+                </div>
+            </div>
+            <!-- buttons -->
+            <div class="form-group w-50 text-right">
+                <input class="btn btn-success mr-4" name="send" type="submit" value="Add Reservation">
+                <input class="btn btn-outline-danger" type="reset" value="Clear">
+            </div>
         </form>
+
     </div>
-
-    <br>
-    <hr />
-
-    <div style='width:600px;float:right;margin-top:25px; margin-right:300px'>
-        <!-- style='margin:auto;width:600px;margin-top:50px;' -->
-        <p>dwdq</p>
-        <h1>Delete a purchase</h1>
-        <!--<form action="insertNewPurchase.php" method="POST"> -->
-        <form action="deletePurchase.php" method="POST">
-            <table>
-                <tr>
-                    <td>Your username:</td>
-                    <td><input name="username" type="text" size='4'><br></td>
-                </tr>
-                <tr>
-                    <td>Date Original:</td>
-                    <td><input name="datePurchase" type="text" size='4'><br></td>
-                </tr>
-                <tr>
-                    <td>Purchase's Date:</td>
-                    <td><input id="datepicker" type="date" name="purchaseDate" placeholder="specify date" class="formAlign" style="display: table-cell;" /></td>
-                </tr>
-                <tr>
-                    <td>Purchase's Code:</td>
-                    <td><input name="date" type="number" size="4"><br></td>
-                </tr>
-                <tr>
-                    <td>
-                        <br>
-                        <input name="send" type="submit" value="DELETE">
-                    </td>
-                    <td>
-                        <br>
-                        <input type="reset" value="CLEAR">
-                    </td>
-                    <td>
-                        <br>
-                        <button onclick=location.href="webErabiltzaileak.php">GO BACK</button>
-                    </td>
-                </tr>
-                <!-- echo "Today is " . date("Y-m-d") . "<br>"; -->
-            </table>
-        </form>
-    </div>
-
-    <!--style='margin:auto;width:600px;margin-top:100px;' -->
-    <!-- style='width:600px;float:left; padding:10%'-->
-    <br><br><br><br><br><br><br><br>
-
-
-    <div style="margin:0;width:100%;">
-        <br><br><br><br><br><br><br><br>
-    </div>
-
-
-
-
-    <br>
-    <br>
-
-
 
 </body>
 
@@ -197,4 +156,3 @@ echo $connection->error;
 }
 
 -->
-
